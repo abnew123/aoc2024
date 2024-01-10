c036b9e5ff3a4094f730521ea687b559f5fb79be
abnew123 <shadowghosts31415@gmail.com>
2024-01-10 21:53:51 +1100

    more initial setup

---
+++ src/solutions/Day09.java
@@ -0,0 +1,19 @@
+            package src.solutions;
+
+            import src.meta.DayTemplate;
+            import src.objects.Coordinate;
+
+            import java.util.*;
+            public class Day09 extends DayTemplate {
+
+                public String solve(boolean part1, Scanner in) {
+
+                    long answer = 0;
+                    List<String[]> tmp = new ArrayList<>();
+                    while (in.hasNext()) {
+                        String line = in.nextLine();
+                        tmp.add(line.split(""));
+                    }
+                    int[][] grid = new int[tmp.get(0).length][tmp.size()];
+                }
+            }
