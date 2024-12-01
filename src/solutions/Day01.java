6a7a0d7d057f652fcccefe714a66bb477bd5d923
abnew123 <shadowghosts31415@gmail.com>
2024-12-01 00:06:57 -0500

    Day 1

--- src/solutions/Day01.java
+++ src/solutions/Day01.java
@@ -1,8 +1,10 @@
 package src.solutions;
 
 import src.meta.DayTemplate;
+import static src.meta.Utils.*;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
@@ -11,13 +13,29 @@ public class Day01 extends DayTemplate {
     public String solve(boolean part1, Scanner in) {
 
         long answer = 0;
-        List<String[]> tmp = new ArrayList<>();
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
         while (in.hasNext()) {
-            String line = in.nextLine();
-            tmp.add(line.split(""));
+            String[] line = in.nextLine().split("   ");
+            left.add(Integer.parseInt(line[0]));
+            right.add(Integer.parseInt(line[1]));
         }
-        int[][] grid = new int[tmp.get(0).length][tmp.size()];
-
-        return "";
+        if(part1){
+            Collections.sort(left);
+            Collections.sort(right);
+            for(int i = 0; i < left.size(); i++){
+                answer += Math.abs(left.get(i) - right.get(i));
+            }
+        }
+        else{
+            for(Integer l: left){
+                for(Integer r: right){
+                    if(r.equals(l)){
+                        answer+=l;
+                    }
+                }
+            }
+        }
+        return answer+ "";
     }
 }
