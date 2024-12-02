b6f6856c4b50ec1a424c84eb20933da231e07e6f
abnew123 <shadowghosts31415@gmail.com>
2024-12-02 00:15:32 -0500

    Day 2

--- src/solutions/Day02.java
+++ src/solutions/Day02.java
@@ -14,10 +14,60 @@ public class Day02 extends DayTemplate {
         List<String[]> tmp = new ArrayList<>();
         while (in.hasNext()) {
             String line = in.nextLine();
-            tmp.add(line.split(""));
+            tmp.add(line.split(" "));
         }
-        int[][] grid = new int[tmp.get(0).length][tmp.size()];
 
-        return "";
+        if(part1){
+            for(String[] arr: tmp){
+                if(safe(arr)){
+                    answer++;
+                }
+            }
+        }
+        else{
+            for(String[] arr: tmp){
+                boolean any = false;
+                for(int i = 0; i < arr.length; i++){
+                    String[] newarr = new String[arr.length - 1];
+                    System.arraycopy(arr, 0, newarr, 0, i);
+                    if (arr.length >= i + 1)
+                        System.arraycopy(arr, i + 1, newarr, i + 1 - 1, arr.length - (i + 1));
+                    if(safe(newarr)){
+                        any = true;
+                    }
+                }
+                if(any){
+                    answer++;
+                }
+            }
+        }
+        return answer + "";
+    }
+
+    private boolean safe(String[] steps){
+        int[] nums = new int[steps.length];
+        for(int i = 0; i < nums.length; i++){
+            nums[i] = Integer.parseInt(steps[i]);
+        }
+        boolean decreasing = nums[0] > nums[1];
+        if(nums[0] == nums[1]){
+            return false;
+        }
+        for(int i = 1; i < nums.length; i++){
+            if(Math.abs(nums[i] - nums[i-1]) > 3){
+                return false;
+            }
+            if(nums[i] > nums[i - 1] && decreasing){
+                return false;
+            }
+            if(nums[i] < nums[i - 1] && !decreasing){
+                return false;
+            }
+            if(nums[i] == nums[i-1]){
+                return false;
+            }
+        }
+        return true;
+
     }
 }
