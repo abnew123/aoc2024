145261f1758ec436136afe11b187df6c05ec60b4
abnew123 <shadowghosts31415@gmail.com>
2024-12-09 01:02:38 -0500

    day 9 cleanup

--- src/solutions/Day09.java
+++ src/solutions/Day09.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class Day09 {
     public String solve(boolean part1, Scanner in) {
-        long answer = 0;
+        long answer;
         String line = in.nextLine();
         int[] parts = new int[line.length()];
         int length = 0;
@@ -12,100 +12,82 @@ public class Day09 {
             parts[i] = Integer.parseInt(line.substring(i, i+1));
             length += parts[i];
         }
+        if(part1){
+            answer = part1(length, parts);
+        }
+        else{
+            answer = part2(parts);
+        }
+        return answer + "";
+    }
+
+    private long part1(int length, int[] parts){
+        long answer = 0;
         int[] filesystem = new int[length];
         int[] condensed = new int[length];
         int index = 0;
         for(int i = 0; i < parts.length; i++){
             int part = parts[i];
             for(int j = 0; j < part; j++){
-                if(i % 2 == 1){
-                    filesystem[index + j] = -1;
-                }
-                else{
-                    filesystem[index + j] = i/2;
-                }
+                filesystem[index + j] = (i % 2 == 1)?-1:i/2;
             }
             index += part;
         }
         int backindex = length - 1;
-        int b = 0;
-        if(part1){
-            for(int i = 0; i < length; i++){
-                if(filesystem[i] != -1){
-                    condensed[i] = filesystem[i];
-//                System.out.println(i);
-//                System.out.println(length);
-                }
-
-                else{
-                    while(backindex >= 0 && filesystem[backindex] == -1){
-                        backindex--;
-                    }
-                    if(backindex > i){
-                        condensed[i] = filesystem[backindex];
-                        System.out.println(backindex);
-                        filesystem[backindex] = -1;
-                        b = i;
-                    }
-
-                }
+        for(int i = 0; i < length; i++){
+            if(filesystem[i] != -1){
+                condensed[i] = filesystem[i];
             }
-            for(int i = 0; i < condensed.length; i++){
-                if(condensed[i] != -1){
-                    answer+= i * condensed[i];
+            else{
+                while(backindex >= i && filesystem[backindex] == -1){
+                    backindex--;
                 }
-
+                if(backindex > i){
+                    condensed[i] = filesystem[backindex];
+                    filesystem[backindex] = -1;
+                }
             }
         }
-        else{
-            Map<Integer, int[]> starting = new HashMap<>();
-            Map<Integer, int[]> gaps = new HashMap<>();
-            int index1 = 0;
-            for(int i = 0; i < parts.length; i++) {
-                if(i %2 == 0){
-                    starting.put(i / 2, new int[]{index1, parts[i]});
-                }
-                if(i % 2 == 1){
-                    gaps.put(i/2, new int[] {index1, parts[i]});
-                }
-                index1 += parts[i];
+        for(int i = 0; i < condensed.length; i++){
+            if(condensed[i] != -1){
+                answer+= (long) i * condensed[i];
             }
-            for(int key: starting.keySet()) {
-                int[] value = starting.get(key);
-                System.out.println(key + " " + Arrays.toString(value));
-            }
+        }
+        return answer;
+    }
 
-            List<Integer> blocks = new ArrayList<>();
-            for(int i = 0; i < parts.length; i+=2){
-                blocks.add(parts[1]);
+    private long part2(int[] parts){
+        long answer = 0;
+        Map<Integer, int[]> starting = new HashMap<>();
+        Map<Integer, int[]> gaps = new HashMap<>();
+        int index = 0;
+        for(int i = 0; i < parts.length; i++) {
+            if(i %2 == 0){
+                starting.put(i / 2, new int[]{index, parts[i]});
             }
-            System.out.println(gaps);
-            for(int i = blocks.size() - 1; i >= 0; i--){
-                int size = starting.get(i)[1];
-                for(int j = 0; j < i; j++){
-                    int gap = gaps.get(j)[1];
-                    if(gap >= size){
-                        starting.put(i, new int[]{gaps.get(j)[0], size});
-                        System.out.println("Moved number " + i +  " to location " + gaps.get(j)[0]);
-                        gaps.put(j, new int[]{gaps.get(j)[0] + size, gaps.get(j)[1] - size});
-                        break;
-                    }
-                }
-                System.out.println(gaps);
+            if(i % 2 == 1){
+                gaps.put(i / 2, new int[]{index, parts[i]});
             }
-
-            for(int key: starting.keySet()){
-                int[] value = starting.get(key);
-                System.out.println(key + " " + Arrays.toString(value));
-                for(int i = value[0]; i < value[0] + value[1]; i++){
-                    answer+= i * key;
+            index += parts[i];
+        }
+        for(int i = parts.length/2 ; i >= 0; i--){
+            int size = starting.get(i)[1];
+            for(int j = 0; j < i; j++){
+                int gap = gaps.get(j)[1];
+                if(gap >= size){
+                    starting.put(i, new int[]{gaps.get(j)[0], size});
+                    gaps.put(j, new int[]{gaps.get(j)[0] + size, gaps.get(j)[1] - size});
+                    break;
                 }
             }
         }
 
-//        System.out.println(Arrays.toString(condensed));
-//        System.out.println(Arrays.toString(filesystem));
-
-        return answer + "";
+        for (Map.Entry<Integer, int[]> entry : starting.entrySet()) {
+            int[] value = entry.getValue();
+            for (int i = value[0]; i < value[0] + value[1]; i++) {
+                answer += (long) i * entry.getKey();
+            }
+        }
+        return answer;
     }
 }
