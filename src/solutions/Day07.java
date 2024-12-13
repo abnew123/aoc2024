280ecdf890040e9be1151024001d87ffdcda8746
abnew123 <shadowghosts31415@gmail.com>
2024-12-13 14:24:18 -0500

    refactor day 7 to make concatentation easier

--- src/solutions/Day07.java
+++ src/solutions/Day07.java
@@ -1,67 +1,61 @@
 package src.solutions;
 
+import src.meta.DayTemplate;
+
 import java.util.*;
 
-public class Day07 {
+public class Day07 extends DayTemplate {
 
     public String solve(boolean part1, Scanner in) {
         long answer = 0;
-        List<long[]> values = new ArrayList<>();
-        List<Long> targets = new ArrayList<>();
+        List<String[]> values = new ArrayList<>();
+        List<String> targets = new ArrayList<>();
         while (in.hasNext()) {
             String line = in.nextLine();
             String[] parts1 = line.split(":");
             String[] parts2 = parts1[1].trim().split(" ");
-            targets.add(Long.parseLong(parts1[0]));
-            long[] tmp = new long[parts2.length];
-            for(int i = 0; i < parts2.length; i++){
-                tmp[i] = Long.parseLong(parts2[i]);
-            }
-            values.add(tmp);
+            targets.add(parts1[0]);
+            values.add(parts2);
         }
-
         for(int i = 0; i < values.size(); i++){
-            answer+= oneCycle(targets.get(i), values.get(i), part1);
+            answer+= Long.parseLong(oneCycle(targets.get(i), values.get(i), part1));
         }
         return answer + "";
     }
 
-    private long oneCycle(long target, long[] values, boolean part1){
-        Set<Long> possible = new HashSet<>();
+    private String oneCycle(String target, String[] values, boolean part1){
+        Set<String> possible = new HashSet<>();
         possible.add(values[0]);
         for(int i = 1; i < values.length; i++){
-            Set<Long> newPossible = new HashSet<>();
-            for(Long p: possible){
-                newPossible.add(operation(0,p,values[i]));
-                newPossible.add(operation(1,p,values[i]));
-                if(!part1){
-                    newPossible.add(operation(2,p,values[i]));
+            Set<String> newPossible = new HashSet<>();
+            for(String p: possible){
+                if(Long.parseLong(p) < Long.parseLong(target)){
+                    newPossible.add(operation(0,p,values[i]));
+                    newPossible.add(operation(1,p,values[i]));
+                    if(!part1){
+                        newPossible.add(operation(2,p,values[i]));
+                    }
                 }
+
             }
             possible = newPossible;
         }
         if(possible.contains(target)){
             return target;
         }
-        return 0;
+        return "0";
     }
 
-    private long operation(int type, long input1, long input2){
+    private String operation(int type, String input1, String input2){
         if(type == 0){
-            return input1 + input2;
+            return Long.parseLong(input1) + Long.parseLong(input2) + "";
         }
         if(type == 1){
-            return input1 * input2;
+            return Long.parseLong(input1) * Long.parseLong(input2) + "";
         }
         if(type == 2){
-            int size = 0;
-            long tmp = input2;
-            while(tmp > 0){
-                tmp/=10;
-                size++;
-            }
-            return input1 * (long)Math.pow(10, size) + input2;
+            return input1 + input2;
         }
-        return -1;
+        return "0";
     }
 }
