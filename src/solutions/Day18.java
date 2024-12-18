c9f4aefd7585f6a02137a7e98348d086044d8524
abnew123 <shadowghosts31415@gmail.com>
2024-12-18 00:22:48 -0500

    Day 18

---
+++ src/solutions/Day18.java
@@ -0,0 +1,61 @@
+package src.solutions;
+
+import src.meta.DayTemplate;
+import src.objects.Coordinate;
+
+import java.util.*;
+
+import static src.meta.Utils.*;
+
+public class Day18 extends DayTemplate {
+
+    public String solve(boolean part1, Scanner in) {
+        int gridSize = 71;
+        int[][] grid = new int[gridSize][gridSize];
+        List<Coordinate> lines = new ArrayList<>();
+        while(in.hasNext()){
+            String[] parts = in.nextLine().split(",");
+            lines.add(new Coordinate(Integer.parseInt(parts[0]), Integer.parseInt(parts[1])));
+        }
+        if(part1){
+            return bfs(1024, lines, grid) + "";
+
+        }
+        else{
+            int time = 0;
+            while(bfs(time, lines, grid) != -1){
+                time++;
+            }
+            return lines.get(time - 1).x + "," + lines.get(time - 1).y;
+        }
+    }
+
+    private int bfs(int limit, List<Coordinate> memoryBlock, int[][] grid){
+        for(int i = 0; i < limit; i++){
+            grid[memoryBlock.get(i).x][memoryBlock.get(i).y] = 1;
+        }
+        Set<Coordinate> lst = new HashSet<>();
+        lst.add(new Coordinate(0,0));
+        int counter = 0;
+        Coordinate end = new Coordinate(grid.length -1, grid.length - 1);
+        Set<Coordinate> allSeen = new HashSet<>();
+        while(!lst.contains(end) && counter < grid.length * grid[0].length * 2){
+            counter++;
+            allSeen.addAll(lst);
+            Set<Coordinate> newList = new HashSet<>();
+            for(Coordinate c: lst){
+                for(Coordinate neighbor: getNeighbors(c.x, c.y, grid)){
+                    if(grid[neighbor.x][neighbor.y] != 1 && !allSeen.contains(neighbor)){
+                        newList.add(neighbor);
+                    }
+                }
+            }
+            lst = newList;
+        }
+        if(counter >= grid.length * grid[0].length){
+            return -1;
+        }
+        return counter;
+
+    }
+}
