06f17d0baa60935c8b18b032adc6f0c6754f85f4
abnew123 <shadowghosts31415@gmail.com>
2026-07-04 23:51:38 -0400

    Optimize Day5 update ordering

--- src/solutions/Day05.java
+++ src/solutions/Day05.java
@@ -2,77 +2,107 @@ package src.solutions;
 
 import src.meta.DayTemplate;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Day05 extends DayTemplate {
-    List<String[]> rules;
+    private final boolean[][] before = new boolean[100][100];
+    private final int[] indexByPage = new int[100];
+    private final boolean[] present = new boolean[100];
 
     public String solve(boolean part1, Scanner in) {
-        long answer = 0;
-        rules = new ArrayList<>();
-        List<String[]> updates = new ArrayList<>();
-        while (in.hasNext()) {
+        List<int[]> updates = new ArrayList<>();
+        List<int[]> rules = new ArrayList<>();
+        for (boolean[] row : before) {
+            java.util.Arrays.fill(row, false);
+        }
+
+        while (in.hasNextLine()) {
             String line = in.nextLine();
-            if(line.contains("|")){
-                rules.add(line.split("\\|"));
-            }
-            if(line.contains(",")){
-                updates.add(line.split(","));
+            if (line.contains("|")) {
+                int separator = line.indexOf('|');
+                int first = Integer.parseInt(line.substring(0, separator));
+                int second = Integer.parseInt(line.substring(separator + 1));
+                before[first][second] = true;
+                rules.add(new int[]{first, second});
+            } else if (line.contains(",")) {
+                String[] split = line.split(",");
+                int[] update = new int[split.length];
+                for (int i = 0; i < split.length; i++) {
+                    update[i] = Integer.parseInt(split[i]);
+                }
+                updates.add(update);
             }
         }
-        for(String[] update : updates){
-            answer+=part1?part1(update):part2(update);
+
+        long answer = 0;
+        for (int[] update : updates) {
+            answer += part1 ? part1(update, rules) : part2(update, rules);
         }
         return answer + "";
     }
 
-    private int part1(String[] update){
-        Map<Integer, Integer> indices = new HashMap<>();
-        for(int i = 0; i < update.length; i++){
-            indices.put(Integer.parseInt(update[i]), i);
-        }
-        for(String[] rule: rules){
-            int first = Integer.parseInt(rule[0]);
-            int second = Integer.parseInt(rule[1]);
-            if(indices.containsKey(first) && indices.containsKey(second) && (indices.get(first) > indices.get(second))) {
-                return 0;
+    private int part1(int[] update, List<int[]> rules) {
+        markUpdate(update);
+        boolean correct = true;
+        for (int[] rule : rules) {
+            int first = rule[0];
+            int second = rule[1];
+            if (present[first] && present[second] && indexByPage[first] > indexByPage[second]) {
+                correct = false;
+                break;
             }
         }
-        return Integer.parseInt(update[update.length/2]);
+        unmarkUpdate(update);
+        return correct ? update[update.length / 2] : 0;
     }
 
-    private int part2(String[] update){
-        Map<Integer, Integer> indices = new HashMap<>();
-        for(int i = 0; i < update.length; i++){
-            indices.put(Integer.parseInt(update[i]), i);
-        }
-        Map<Integer, List<Integer>> priors = new HashMap<>();
+    private int part2(int[] update, List<int[]> rules) {
+        markUpdate(update);
         boolean incorrect = false;
-        for(String[] rule: rules){
-            int first = Integer.parseInt(rule[0]);
-            int second = Integer.parseInt(rule[1]);
-            if(indices.containsKey(first) && indices.containsKey(second)){
-                priors.computeIfAbsent(first, k -> new ArrayList<>()).add(second);
-                if(indices.get(first) > indices.get(second)) {
-                    incorrect = true;
-                }
+        for (int[] rule : rules) {
+            int first = rule[0];
+            int second = rule[1];
+            if (present[first] && present[second] && indexByPage[first] > indexByPage[second]) {
+                incorrect = true;
+                break;
             }
         }
-        if(incorrect){
-            List<Integer> ordered = new ArrayList<>();
-            while(ordered.size() < update.length ){
-                for(int val: indices.keySet()){
-                    if(priors.getOrDefault(val, new ArrayList<>()).isEmpty() && !ordered.contains(val)){
-                        ordered.add(val);
-                    }
-                }
-                // for better perf you can remove only the newly added elements, but all updates are relatively small in this case
-                for(int val: priors.keySet()){
-                    priors.computeIfAbsent(val, k -> new ArrayList<>()).removeAll(ordered);
-                }
-            }
-            return ordered.get(ordered.size()/2);
+        unmarkUpdate(update);
+        if (!incorrect) {
+            return 0;
+        }
+
+        Integer[] ordered = new Integer[update.length];
+        for (int i = 0; i < update.length; i++) {
+            ordered[i] = update[i];
+        }
+        java.util.Arrays.sort(ordered, this::comparePages);
+        return ordered[ordered.length / 2];
+    }
+
+    private int comparePages(int first, int second) {
+        if (before[first][second]) {
+            return -1;
+        }
+        if (before[second][first]) {
+            return 1;
         }
         return 0;
+    }
+
+    private void markUpdate(int[] update) {
+        for (int i = 0; i < update.length; i++) {
+            int page = update[i];
+            present[page] = true;
+            indexByPage[page] = i;
+        }
+    }
+
+    private void unmarkUpdate(int[] update) {
+        for (int page : update) {
+            present[page] = false;
+        }
     }
 }
