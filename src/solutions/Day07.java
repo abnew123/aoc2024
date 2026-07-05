05c6882725145d33b65892714f62d9d316f5d394
abnew123 <shadowghosts31415@gmail.com>
2026-07-05 01:59:17 -0400

    Optimize Day7 calibration recursion

--- src/solutions/Day07.java
+++ src/solutions/Day07.java
@@ -2,54 +2,64 @@ package src.solutions;
 
 import src.meta.DayTemplate;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Day07 extends DayTemplate {
 
     public String solve(boolean part1, Scanner in) {
         long answer = 0;
-        List<List<Integer>> equations = new ArrayList<>();
+        List<int[]> equations = new ArrayList<>();
         List<Long> targets = new ArrayList<>();
-        while (in.hasNext()) {
+        while (in.hasNextLine()) {
             String line = in.nextLine();
-            String[] parts1 = line.split(":");
-            String[] parts2 = parts1[1].trim().split(" ");
-            targets.add(Long.parseLong(parts1[0]));
-            List<Integer> equation = new ArrayList<>();
-            for(String p: parts2){
-                equation.add(Integer.parseInt(p));
+            int colon = line.indexOf(':');
+            targets.add(Long.parseLong(line.substring(0, colon)));
+            String[] parts = line.substring(colon + 2).split(" ");
+            int[] equation = new int[parts.length];
+            for (int i = 0; i < parts.length; i++) {
+                equation[i] = Integer.parseInt(parts[i]);
             }
             equations.add(equation);
         }
 
-        for(int i = 0; i < targets.size(); i++){
+        for (int i = 0; i < targets.size(); i++) {
             long target = targets.get(i);
-            if(possible(target, equations.get(i), part1)){
-                answer+= target;
+            if (possible(target, equations.get(i), equations.get(i).length - 1, part1)) {
+                answer += target;
             }
         }
 
         return answer + "";
     }
 
-    private boolean possible(long target, List<Integer> equation, boolean part1){
-        if(equation.isEmpty()){
+    private boolean possible(long target, int[] equation, int index, boolean part1) {
+        if (index < 0) {
             return target == 0;
         }
-        int nextVal = equation.get(equation.size() - 1);
-        if(target < nextVal){
+        int nextVal = equation[index];
+        if (target < nextVal) {
             return false;
         }
-        List<Integer> reducedEquation = new ArrayList<>(equation);
-        reducedEquation.remove(equation.size() - 1);
-        boolean possible = possible(target - nextVal, reducedEquation, part1);
-        if(target % nextVal == 0){
-            possible |= possible(target/nextVal, reducedEquation, part1);
+        if (possible(target - nextVal, equation, index - 1, part1)) {
+            return true;
         }
-        int size = (nextVal+"").length();
-        if((target+"").endsWith(nextVal+"") && !part1){
-            possible |= possible(target/ (int)(Math.pow(10,size)), reducedEquation, part1);
+        if (target % nextVal == 0 && possible(target / nextVal, equation, index - 1, part1)) {
+            return true;
         }
-        return possible;
+        if (!part1) {
+            int divisor = pow10(nextVal);
+            return target % divisor == nextVal && possible(target / divisor, equation, index - 1, part1);
+        }
+        return false;
+    }
+
+    private int pow10(int value) {
+        int result = 10;
+        while (value >= result) {
+            result *= 10;
+        }
+        return result;
     }
 }
