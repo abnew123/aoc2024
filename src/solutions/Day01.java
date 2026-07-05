40cac9ecc69d8f0dc75ee7ef3073e75a78a0dd3f
abnew123 <shadowghosts31415@gmail.com>
2026-07-04 23:53:07 -0400

    Optimize Day1 and Day4 parsing

--- src/solutions/Day01.java
+++ src/solutions/Day01.java
@@ -4,37 +4,39 @@ import src.meta.DayTemplate;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Day01 extends DayTemplate {
 
     public String solve(boolean part1, Scanner in) {
-
-        long answer = 0;
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
-        while (in.hasNext()) {
-            String[] line = in.nextLine().split("   ");
-            left.add(Integer.parseInt(line[0]));
-            right.add(Integer.parseInt(line[1]));
+        Map<Integer, Integer> rightCounts = new HashMap<>();
+        while (in.hasNextLine()) {
+            String line = in.nextLine();
+            int separator = line.indexOf(' ');
+            int leftValue = Integer.parseInt(line.substring(0, separator));
+            int rightValue = Integer.parseInt(line.substring(line.lastIndexOf(' ') + 1));
+            left.add(leftValue);
+            right.add(rightValue);
+            rightCounts.merge(rightValue, 1, Integer::sum);
         }
-        if(part1){
+
+        long answer = 0;
+        if (part1) {
             Collections.sort(left);
             Collections.sort(right);
-            for(int i = 0; i < left.size(); i++){
+            for (int i = 0; i < left.size(); i++) {
                 answer += Math.abs(left.get(i) - right.get(i));
             }
-        }
-        else{
-            for(Integer l: left){
-                for(Integer r: right){
-                    if(r.equals(l)){
-                        answer+=l;
-                    }
-                }
+        } else {
+            for (int value : left) {
+                answer += (long) value * rightCounts.getOrDefault(value, 0);
             }
         }
-        return answer+ "";
+        return answer + "";
     }
 }
