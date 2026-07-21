cb41c4d7fff15ef40a07307405a62c0a94ddf574
abnew123 <shadowghosts31415@gmail.com>
2026-07-20 23:45:58 -0400

    Golf another 28 characters

--- src/solutions/Day25Golfed.java
+++ src/solutions/Day25Golfed.java
@@ -1,1 +1,1 @@
-class Y{String s(boolean p,String[]S){int r=0,i,j,k;for(j=0;j<S.length;j+=8)for(k=j+8;k<S.length;k+=8,r-=i>>31)for(i=35;i-->0&&(S[j+i/5].charAt(i%5)&S[k+i/5].charAt(i%5)&1)<1;);return r+"";}}
+class Y{String s(boolean p,String[]S){int r=0,i,j=0,k;for(;j<S.length;j+=8)for(k=j;(k+=8)<S.length;r-=i>>31)for(i=35;i-->0&&(S[j+i/5].charAt(i%5)&S[k+i/5].charAt(i%5)&1)<1;);return r+"";}}
