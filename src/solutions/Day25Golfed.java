bad606f4884fe7caa0f104c69a6e92f0d0c5b272
abnew123 <shadowghosts31415@gmail.com>
2026-07-24 20:39:08 -0400

    Golf 11 days for 94 characters (small-file sweep + B=C=0 on day 17, hardcoded grids on days 12/15)

--- src/solutions/Day25Golfed.java
+++ src/solutions/Day25Golfed.java
@@ -1,1 +1,1 @@
-class Y{String s(boolean p,String[]S){int r=0,i,j=0,k;for(;j<S.length;j+=8)for(k=j;(k+=8)<S.length;r-=i>>31)for(i=35;i-->0&&(S[j+i/5].charAt(i%5)&S[k+i/5].charAt(i%5)&1)<1;);return r+"";}}
+class Y{String s(boolean p,String[]S){int r=0,i,j=0,k,n=S.length;for(;j<n;j+=8)for(k=j;(k+=8)<n;r-=i>>6)for(i=35;i-->0&&(S[j+i/5].charAt(i%5)&S[k+i/5].charAt(i%5)&1)<1;);return r+"";}}
